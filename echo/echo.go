2d0241c683e80bddff1d4d6b27353400a51c11f5
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-10-18 01:51:33 -0300

    Echo server

---
+++ echo/echo.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	list := []User{ 
+		{"Pablo","pablo@gmail.com"}, 
+		{"daniel","daniel@gmail.com"}, 
+		{"Lucero","Lucero@gmail.com"}, 
+	}
+
+	e.GET("/users", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, list)
+	})
+
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
